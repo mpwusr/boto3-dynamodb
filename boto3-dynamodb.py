e00e8a95928f91950d65d3cc74728eadd8448838
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-04-25 10:33:59 -0400

    Update boto3-dynamodb.py

--- boto3-dynamodb.py
+++ boto3-dynamodb.py
@@ -1,1 +1,227 @@
 
+import boto3
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='users')
+
+# Print out some data about the table.
+print(table.item_count)
+
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('users')
+
+# Print out some data about the table.
+# This will cause a request to be made to DynamoDB and its attribute
+# values will be set based on the response.
+print(table.creation_date_time)
+
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
+
+response = table.get_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    }
+)
+item = response['Item']
+print(item
+      
+      table.update_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    },
+    UpdateExpression='SET age = :val1',
+    ExpressionAttributeValues={
+        ':val1': 26
+    }
+)
+      response = table.get_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    }
+)
+item = response['Item']
+print(item)
+      
+      table.delete_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    }
+)
+      with table.batch_writer() as batch:
+    batch.put_item(
+        Item={
+            'account_type': 'standard_user',
+            'username': 'johndoe',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'age': 25,
+            'address': {
+                'road': '1 Jefferson Street',
+                'city': 'Los Angeles',
+                'state': 'CA',
+                'zipcode': 90001
+            }
+        }
+    )
+    batch.put_item(
+        Item={
+            'account_type': 'super_user',
+            'username': 'janedoering',
+            'first_name': 'Jane',
+            'last_name': 'Doering',
+            'age': 40,
+            'address': {
+                'road': '2 Washington Avenue',
+                'city': 'Seattle',
+                'state': 'WA',
+                'zipcode': 98109
+            }
+        }
+    )
+    batch.put_item(
+        Item={
+            'account_type': 'standard_user',
+            'username': 'bobsmith',
+            'first_name': 'Bob',
+            'last_name':  'Smith',
+            'age': 18,
+            'address': {
+                'road': '3 Madison Lane',
+                'city': 'Louisville',
+                'state': 'KY',
+                'zipcode': 40213
+            }
+        }
+    )
+    batch.put_item(
+        Item={
+            'account_type': 'super_user',
+            'username': 'alicedoe',
+            'first_name': 'Alice',
+            'last_name': 'Doe',
+            'age': 27,
+            'address': {
+                'road': '1 Jefferson Street',
+                'city': 'Los Angeles',
+                'state': 'CA',
+                'zipcode': 90001
+            }
+        }
+    )
+      
+      with table.batch_writer() as batch:
+    for i in range(50):
+        batch.put_item(
+            Item={
+                'account_type': 'anonymous',
+                'username': 'user' + str(i),
+                'first_name': 'unknown',
+                'last_name': 'unknown'
+            }
+        )
+      with table.batch_writer(overwrite_by_pkeys=['partition_key', 'sort_key']) as batch:
+    batch.put_item(
+        Item={
+            'partition_key': 'p1',
+            'sort_key': 's1',
+            'other': '111',
+        }
+    )
+    batch.put_item(
+        Item={
+            'partition_key': 'p1',
+            'sort_key': 's1',
+            'other': '222',
+        }
+    )
+    batch.delete_item(
+        Key={
+            'partition_key': 'p1',
+            'sort_key': 's2'
+        }
+    )
+    batch.put_item(
+        Item={
+            'partition_key': 'p1',
+            'sort_key': 's2',
+            'other': '444',
+        }
+    )
+      batch.put_item(
+    Item={
+        'partition_key': 'p1',
+        'sort_key': 's1',
+        'other': '222',
+    }
+)
+batch.put_item(
+    Item={
+        'partition_key': 'p1',
+        'sort_key': 's2',
+        'other': '444',
+    }
+)
+      
+from boto3.dynamodb.conditions import Key, Attr
+
+
+response = table.query(
+    KeyConditionExpression=Key('username').eq('johndoe')
+)
+items = response['Items']
+print(items)
+      
+      response = table.scan(
+    FilterExpression=Attr('address.state').eq('CA')
+)
+items = response['Items']
+print(items)
